19cbcd134a52d2b01e8f54984696bcbcd19c979f
Ejima-we <takumi941503@gmail.com>
2021-02-16 12:57:21 +0000

    [Finish]2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '3f3194d7a3edde265fb237189cecb1a25160c8ce2bf235781049de7d3487640947fc5f03f977e48bcb5f248ff031562f4a8a72b720bb0a2387d805aca34c2bd2'
